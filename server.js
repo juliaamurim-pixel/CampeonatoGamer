cc660891e3f0dd187e4b093d1014c14f722d2d48
JULIA LIMA AMURIM <26171659@edu119.local>
2026-09-24 11:54:29 -0300

    Subindo projeto

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors()); //middleware
+app.use(express.json());
+
+let PARTIDAS =[
+    {
+        id: 1,
+        jogo: "Arena Pixel", 
+        timeA: "Falcões",
+        timeB: "Dragões",
+        pontoA: 3,
+        pontoB: 2,
+        status: "finalizada"
+    },
+    {
+        id: 2,
+        jogo: "Corrida Turbo", 
+        timeA: "Lobos",
+        timeB: "Falcões",
+        pontoA: 0,
+        pontoB: 0,
+        status: "agendada"
+    },
+];
+
+app.get("/",(req, res)=>{
+    res.status(200).json({mensagem: "Campeonato Gamer no ar!"});
+});
+
+app.get("/partidas",(req,res)=>{
+    res.status(200).json(PARTIDAS);
+});
+
+app.get("/partidas/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const partidas = PARTIDAS.find(p => p.id === id);
+
+    if(!partidas){
+        return res.status(404).json({mensagem: "Partida não encontrada"});
+    }
+
+    res.status(200).json(partidas);
+})
+
+const PORT = 3000
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
